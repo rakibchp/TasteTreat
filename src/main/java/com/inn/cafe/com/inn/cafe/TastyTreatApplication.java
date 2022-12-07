0a6c8b4ce6ad833c351591dcf37315bff281380d
Abdur Rakib <rakibchp@gmail.com>
2022-12-07 15:10:21 +0600

    Project packeges only and aplication properties updates

---
+++ src/main/java/com/inn/cafe/com/inn/cafe/TastyTreatApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyTreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyTreatApplication.class, args);
+	}
+
+}
